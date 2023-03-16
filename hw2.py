efc01e0735638f60d59b6ca51ea1a0615e21ac55
rladlsrua794 <84882961+rladlsrua794@users.noreply.github.com>
2023-03-16 13:44:55 +0900

    Add files via upload

---
+++ hw2.py
@@ -0,0 +1,19 @@
+def exchange(n) :
+    n1 = n // 500
+    print("500원 동전의 개수 : ", n1)
+    n %= 500
+    n2 = n // 100
+    print("100원 동전의 개수 : ", n2)
+    n %= 100
+    n3 = n // 50
+    print("50원 동전의 개수 : ", n3)
+    n %= 50
+    n4 = n // 10
+    print("10원 동전의 개수 : ", n4)
+
+def get_integer() :
+    change = int(input("동전으로 교환하고자 하는 금액은? "))
+    return change
+
+n = get_integer()
+exchange(n)
