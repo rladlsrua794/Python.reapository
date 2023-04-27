c424c3be3deb288421ef8e2b23dd3572d6b060a0
rladlsrua794 <84882961+rladlsrua794@users.noreply.github.com>
2023-04-27 14:37:41 +0900

    Add files via upload

---
+++ hw7.py
@@ -0,0 +1,15 @@
+shopping_bag = {}
+while True :
+    item = input('상품명? ')
+
+    if item == '' :
+        break
+    else :
+        num = input('수량은? ')
+        shopping_bag[item] = num
+        print(f'장바구니에 {item} {num}개가 답겼습니다.')
+
+print('장바구니 보기:', shopping_bag)
+
+find = input('장바구니에서 확인하고자 하는 상품은? ')
+print(f'{find}은(는) {shopping_bag[find]}개 담겨 있습니다.')
